a876b29172cbf9b97a4c67e49bab2c56685771a1
ThanaponB <thanaponb@arise.tech>
2024-04-12 22:44:13 +0700

    arrow function

---
+++ ff.js
@@ -0,0 +1,14 @@
+fullname=(fname,lname)=>fname+lname
+
+setAge=(age)=>"อายุ ="+age 
+
+
+countNum =(number1,number2) => {
+    console.log(number1+number2)
+}
+
+countNum(1,2)
+
+console.log(fullname("Pakkad"," love mony"));
+
+console.log(setAge(20))
